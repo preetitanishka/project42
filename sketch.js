2ae225810fc036f0fb42e9b02a74f5204be152bd
preetitanishka <71656803+preetitanishka@users.noreply.github.com>
2021-02-16 16:52:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,26 +12,26 @@ function draw() {
  mn = minute();
  sc = second();
   scAngle = Map(sc,0,60,0,360);
-  Push();
+  push();
   rotate(scAngle);
   stroke(255,0,0);
   strokeWeight(7);
   line(0,0,100,0);
-  Pop();
+  pop();
   
- mnAngle = Map(sc,0,60,0,360);
-  Push();
+ mnAngle = Map(mn,0,60,0,360);
+  push();
   rotate(mnAngle);
   stroke(255,0,0);
   strokeWeight(7);
   line(0,0,100,0);
-  Pop();
+  pop();
  
- hrAngle = Map(sc,0,60,0,360);
-  Push();
+ hrAngle = Map(hr%,12,0,10,0,360);
+  push();
   rotate(hrAngle);
   stroke(255,0,0);
   strokeWeight(7);
   line(0,0,100,0);
-  Pop();
+  pop();
 }
